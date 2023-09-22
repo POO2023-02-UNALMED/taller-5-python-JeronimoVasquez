e0dcb25d0483565b21abe08a94fdf24b986a9ba2
JeronimoVasquez <jeronimovasquez1307@gmail.com>
2023-09-21 23:18:00 -0500

    Desarrolllo

---
+++ .github/zooAnimales/animal.py
@@ -0,0 +1,61 @@
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.ave import Ave
+class Animal():
+
+    _totalanimales = 0
+    
+    def __init__(self, nombre, edad, habitat, genero):
+        self._nombre = nombre
+        self._edad = edad
+        self._habitat = habitat
+        self._genero = genero
+        self._zona = None
+    @classmethod  
+    def totalPorTipo(self):
+        return f"""Mamiferos: {Mamifero.cantidadMamiferos()}\nAves: {Ave.cantidadAves()}\n
+        Reptiles: {Reptil.cantidad()}\nPeces: {Pez.cantidad()}\nAnfibios: {Anfibio.cantidad()}"""
+    
+        
+    def __str__(self):
+        if self.getZona != None:
+            return f"""Mi nombre es {self.getNombre()}, tengo una edad de {self.getEdad()}
+            , habito en {self.getHabitat()} y mi genero es {self.getGenero()}, la zona en
+            la que me ubico es {self.getZona()}, en el {self.getZona().getZoo()}"""
+        
+        else:
+
+            return f"""Mi nombre es {self.getNombre()}, tengo una edad de {self.getEdad()}
+            , habito en {self.getHabitat()} y mi genero es {self.getGenero()}"""
+    
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getEdad(self):
+        return self._edad
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getHabitat(self):
+        return self._habitat
+    def setHabitat(self, habitat):
+        self._habitat = habitat
+    
+    def getGenero(self):
+        return self._genero
+    def setGenero(self, genero):
+        self._genero = genero
+    
+    def getZona(self):
+        return self._zona
+    def setZona(self, zona):
+        self._zona = zona
+
+    @classmethod
+    def getTotalAnimales(cls):
+        return cls._totalanimales
+    
+    @classmethod
+    def setTotalAnimales(cls, total):
+        cls._totalanimales = total
