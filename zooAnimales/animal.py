81d1d9ed7dac77bc88a508ccf44f815797ddda1d
JeronimoVasquez <jeronimovasquez1307@gmail.com>
2023-09-22 00:27:03 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,4 +1,9 @@
-
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.ave import Ave
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.pez import Pez
+from zooAnimales.reptil import Reptil
+from zooAnimales.animal import Animal
 class Animal():
 
     _totalanimales = 0
